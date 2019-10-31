a349fac15f6b4883fec59e226b78bad607f3b6dc
Lone_Wolf <lonewolf04361@gmail.com>
2019-10-31 14:07:23 -0700

    Add 'texture_changes' mod (#2)
    * Add texture pack by 'That guy from the band wings' and a few texture mods
    
    * Add code license

---
+++ texture_changes/init.lua
@@ -0,0 +1,46 @@
+minetest.register_on_mods_loaded(function()
+	for name, def in pairs(minetest.registered_nodes) do
+		if def.tiles then
+			for key, texture in ipairs(def.tiles) do
+				if type(texture) == "string" then
+					def.tiles[key] = "(" .. texture .. ")" .. get_color(texture)
+				elseif type(texture) == "table" then
+					if texture.image then
+						def.tiles[key].image = "(" .. texture.image..")" .. get_color(texture.image)
+					elseif texture.name then
+						def.tiles[key].name = "(" .. texture.name..")" .. get_color(texture.name)
+					end
+				end
+			end
+
+			minetest.override_item(name, {def.tiles})
+		elseif def.special_tiles then
+			for key, texture in ipairs(def.special_tiles) do
+				if type(texture) == "string" then
+					def.special_tiles[key] = "(" .. texture .. ")" .. get_color(texture)
+				elseif type(texture) == "table" then
+					if texture.image then
+						def.special_tiles[key].image = "(" .. texture.image..")" .. get_color(texture.image)
+					elseif texture.name then
+						def.special_tiles[key].name = "(" .. texture.name..")" .. get_color(texture.name)
+					end
+				end
+			end
+
+			minetest.override_item(name, {def.special_tiles})
+		end
+	end
+	
+	minetest.override_item("default:water_source", {post_effect_color = {a = 150, r = 67, g = 13, b = 13}})
+	minetest.override_item("default:water_flowing", {post_effect_color = {a = 150, r = 67, g = 13, b = 13}})
+end)
+
+function get_color(texture)
+	if texture:find("tree") and not (texture:find("aspen") or texture:find("leaves")) then -- Change trees to a dark grey/brown
+		return "^[colorize:#171716:111"
+	elseif texture:find("sand") then -- Make sands a bit darker too
+		return "^[colorize:#4c4238:100"
+	else -- Color a little orange
+		return "^[colorize:#9b6300:40"
+	end
+end
